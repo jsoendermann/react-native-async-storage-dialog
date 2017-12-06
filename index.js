3929f27ddc5d2698b4f8084864ef755d5cf76f84
Jan Soendermann <jan.soendermann+git@gmail.com>
2017-12-06 09:55:23 +0800

    Fix exports and imports and property sorting

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import { AsyncStorage } from 'react-native'
+const { AsyncStorage } = require('react-native')
 
-export default async () => {
+module.exports = async () => {
   const keys = await AsyncStorage.getAllKeys()
   const array = await AsyncStorage.multiGet(keys)
   const obj = {}
-  array.sort((a, b) => b[1].length - a[1].length).forEach(([key, value]) => {
+  array.sort((a, b) => a[1].length - b[1].length).forEach(([key, value]) => {
     try {
       obj[key] = JSON.parse(value)
     } catch (e) {
